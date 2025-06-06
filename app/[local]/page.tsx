deec621a5336e5755752cb310c948e286184d6e2
SafidyRamaroson <safidyramaroson.patrick@gmail.com>
2025-06-06 12:03:05 +0300

    Add welcome message and description to Home page

--- app/[local]/page.tsx
+++ app/[local]/page.tsx
@@ -5,9 +5,17 @@ import { Header } from "@/components/organisms/Header";
 export default function Home() {
   return (
     <div className="w-screen min-h-screen bg-background">
-       <div className="w-4/5 mx-auto h-full">
-          <Header/>
-       </div>
+      <div className="w-4/5 mx-auto h-full">
+        <Header />
+        <h1 className="text-center font-semibold text-transparent bg-clip-text bg-gradient-to-r from-transparent via-primary to-transparent text-3xl mt-12 mb-4">
+          Bienvenue sur QuoteAI !
+        </h1>
+
+        <p className="text-center text-sm">
+          En quête d’inspiration pour une scène, un personnage ou une punchline mémorable ? <br /> Glissez quelques mots, un thème, une émotion — l’outil vous souffle <br /> la citation qui fera vibrer vos lecteurs ✍️✨.
+        </p>
+        
+      </div>
     </div>
   );
 }
