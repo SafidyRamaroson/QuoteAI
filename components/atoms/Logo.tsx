285e07e8927eb50097376e892404ae955b1b237c
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-06-28 18:11:53 +0200

    Update Logo component to use new logo image and add favicon files.

--- components/atoms/Logo.tsx
+++ components/atoms/Logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function Logo(){
     return(
         <Image
-        src='/assets/animatedQuote.gif'
+        src='/assets/logo.png'
         alt="Quote AI Logo"
         width={50}
         height={50}
